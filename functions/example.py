086373f2fb3e8785cb567a8eaea01f32200ec46f
darionyaphet <darion.yaphet@gmail.com>
2015-06-26 23:36:42 +0800

    coding daily

---
+++ functions/example.py
@@ -0,0 +1,32 @@
+'''
+all(iterable)
+Return True if all elements of the iterable are true
+'''
+
+print 'Test all([1,2,3,4,5]) : '+str(all([1,2,3,4,5]))
+
+'''
+any(iterable)
+Return True if any element of the iterable is true.
+'''
+
+
+'''
+max(iterable, *[, key, default])
+max(arg1, arg2, *args[, key])
+Return the largest item in an iterable or the largest of two or more arguments.
+'''
+print 'Test max([3,5,2,1,9]) : '+str(max([3,5,2,1,9]))
+
+'''
+min(iterable, *[, key, default])
+min(arg1, arg2, *args[, key])
+Return the smallest item in an iterable or the smallest of two or more arguments.
+'''
+print 'Test min([3,5,2,1,9]) : '+str(min([3,5,2,1,9]))
+
+'''
+classmethod(function)
+Return a class method for function.
+'''
+print 
