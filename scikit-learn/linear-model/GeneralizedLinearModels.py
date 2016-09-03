ed992b38b3c74560d573cdf055ce1ea39bf30393
darionyaphet <darion.yaphet@gmail.com>
2016-09-03 22:28:23 +0800

    matplotlib animation and line examples

---
+++ scikit-learn/linear-model/GeneralizedLinearModels.py
@@ -0,0 +1,11 @@
+'''
+Generalized Linear Models
+http://scikit-learn.org/stable/modules/linear_model.html
+'''
+
+from sklearn import linear_model
+
+clf = linear_model.LinearRegression()
+clf.fit ([[0, 0], [1, 1], [2, 2]], [0, 1, 2])
+
+print(clf.coef_)
